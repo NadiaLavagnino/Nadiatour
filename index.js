39b13b9b4951dcc086c35dba98c29135c7c0a354
Nadia Lavagnino <nadia.lavagninoalejandra@gmail.com>
2022-09-11 20:56:06 -0300

    simulador interactivo

---
+++ index.js
@@ -0,0 +1,53 @@
+// let usuario = prompt("Ingrese su nombre")
+// const FRASE = "Bienvenida/o"+ " " + usuario + " " + "a Trasnporte Solari"
+
+// alert(FRASE)
+// console.log(usuario)
+
+
+
+// function multiplicar (valorMinimo, cantidadKm){
+//     let multiplicar = valorMinimo * cantidadKm;
+//     return resultado;
+// }
+// function mostrarResultado (resultado){
+//     alert("Su viaje costará" + resultado)
+// }
+
+// function mostrarMenu (){
+//     let opcion= prompt (" Seleccione un destino,(ESC para salir) \n 1. 20km \n 2. 50km \n 3. 100km ")
+//     return opcion;
+// }
+
+// function cotizar(){
+//     let opcionSeleccionada = mostrarMenu()
+//     while(opcionSeleccionada!= "ESC"){
+//     if(opcionSeleccionada !== ""){
+//         alert("Este es el monto del viaje minimo")
+
+
+//         opcionSeleccionada= parseInt(opcionSeleccionada);
+//     }
+
+//     if (isNaN (opcionSeleccionada)){
+//         switch (opcionSeleccionada){
+
+//             case 1:
+//                 let resultadoMultiplicar = multiplicar(valorMinimo, cantidadKm);
+//                 const valorMinimo=500
+//                 mostrarResultado(resultadoMultiplicar);
+//                 break;
+//         }
+//     }
+    
+// }
+// }
+// opcionSeleccionada= mostrarMenu
+// cotizar()
+// multiplicar()
+
+
+let frutas= ["manzana","banana","pera"]
+{
+    console.log(frutas[3])
+}
